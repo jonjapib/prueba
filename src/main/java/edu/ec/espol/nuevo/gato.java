6a0b51ca9b437b45635ccb2d607e9b0676a1a7be
jonjapib <jonjapib@espol.edu.ec>
2021-10-30 23:25:09 -0500

    prueba

---
+++ src/main/java/edu/ec/espol/nuevo/gato.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ec.espol.nuevo;
+
+/**
+ *
+ * @author Pibaque Ponce
+ */
+public class gato {
+    String gato;
+    String apodo;
+    public gato(){
+        this.apodo=apodo;
+    }
+    
+}
